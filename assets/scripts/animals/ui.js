91f5fbc56210c8d1a8fbba6a706fa16b271a7eda
Katie Veneziano <kmveneziano@comcast.net>
2020-11-21 16:36:08 -0500

    TEST 2 of update
    Editing the params

--- assets/scripts/animals/ui.js
+++ assets/scripts/animals/ui.js
@@ -21,7 +21,7 @@ const onNewAnimalFailure = function () {
 
 // This runs if updating an animal resource was successful.
 const onUpdateAnimalSuccess = function (response) {
-  $('#intro2').text('Woof! No problem, ' + store.user.email + '.')
+  $('#intro2').text('Woof! No problem.')
   console.log('It updated')
   $('#update-animal').trigger('reset')
   $('#intro').hide()
