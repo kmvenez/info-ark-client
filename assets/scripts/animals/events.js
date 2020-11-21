67ac853ce14a6647da9ea39d4cea43d3605c5d25
Katie Veneziano <kmveneziano@comcast.net>
2020-11-21 17:31:01 -0500

    More update work

--- assets/scripts/animals/events.js
+++ assets/scripts/animals/events.js
@@ -19,6 +19,7 @@ const onUpdateAnimal = function (event) {
   event.preventDefault()
   const form = event.target
   const data = getFormFields(form)
+  console.log(data)
   api.updateAnimal(data)
     .then(ui.onUpdateAnimalSuccess)
     .catch(ui.onUpdateAnimalFailure)
